6cf033ae8adccc2b702fbb2b6caccc1c9f6179ab
CAWI\User <cawicaancornelio@gmail.com>
2020-02-06 22:54:25 +1300

    Added simple injector

---
+++ Lychee.HttpClientService/Lychee.HttpClientService/HttpClientServiceModule.cs
@@ -0,0 +1,13 @@
+﻿using SimpleInjector;
+
+namespace Lychee.HttpClientService
+{
+    public class HttpClientServiceModule
+    {
+        public void Register(Container container)
+        {
+            container.RegisterSingleton<IHttpClientProvider, HttpClientProvider>();
+            container.RegisterSingleton<IHttpClientService, HttpClientService>();
+        }
+    }
+}
